e7cf5e4a06ab1836c10da5ac3d31c28123c24d0d
ritika7893 <ritikakumari87344@gmail.com>
2025-04-22 22:44:37 +0530

    User Profiles

--- userauth/models.py
+++ userauth/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+from django.conf import settings
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=250, unique=True)
+    name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=True)  # Fixed typo from is_activate
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = "email"  # Changed from plural to singular
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):  # Fixed from three underscores to two
+        return self.email
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
+    
--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from userauth import serializers, models
 
-# Create your views here.
+# from userauth import permissions  # Uncomment if you add custom permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
